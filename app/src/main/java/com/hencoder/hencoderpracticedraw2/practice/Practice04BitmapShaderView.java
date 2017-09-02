86d00d5669d59ff93fe52722219f4a16ebefe5c0
zhengjiong <zhengjiong2006@gmail.com>
2017-09-02 15:26:57 +0800

    feat<Practice>finish 04 05

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice04BitmapShaderView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice04BitmapShaderView.java
@@ -1,14 +1,21 @@
 package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.hencoder.hencoderpracticedraw2.R;
+
 public class Practice04BitmapShaderView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Shader shader;
 
     public Practice04BitmapShaderView(Context context) {
         super(context);
@@ -25,12 +32,16 @@ public class Practice04BitmapShaderView extends View {
     {
         // 用 Paint.setShader(shader) 设置一个 BitmapShader
         // Bitmap: R.drawable.batman
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+        shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        paint.setShader(shader);
         canvas.drawCircle(200, 200, 200, paint);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice05ComposeShaderView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice05ComposeShaderView.java
@@ -1,14 +1,23 @@
 package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.ComposeShader;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.hencoder.hencoderpracticedraw2.R;
+
 public class Practice05ComposeShaderView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Shader shader;
 
     public Practice05ComposeShaderView(Context context) {
         super(context);
@@ -28,12 +37,26 @@ public class Practice05ComposeShaderView extends View {
         // 用 Paint.setShader(shader) 设置一个 ComposeShader
         // Shader 1: BitmapShader 图片：R.drawable.batman
         // Shader 2: BitmapShader 图片：R.drawable.batman_logo
+
+        Bitmap bitmapA = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+        Bitmap bitmapB = BitmapFactory.decodeResource(getResources(), R.drawable.batman_logo);
+
+        Shader shaderA = new BitmapShader(bitmapA, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        Shader shaderB = new BitmapShader(bitmapB, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+
+        /**
+         * http://hencoder.com/ui-1-2/
+         * DST_IN 蒙版抠图效果
+         */
+        shader = new ComposeShader(shaderA, shaderB, PorterDuff.Mode.DST_IN);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+
+        paint.setShader(shader);
         canvas.drawCircle(200, 200, 200, paint);
     }
 }
