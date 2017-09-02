70987d57eb94d6fd99933fd5497756ca3c5cf500
zhengjiong <zhengjiong2006@gmail.com>
2017-09-02 14:09:25 +0800

    feat<Practice>finish 01

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice01LinearGradientView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice01LinearGradientView.java
@@ -3,13 +3,30 @@ package com.hencoder.hencoderpracticedraw2.practice;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice01LinearGradientView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    /**
+     * 参数：
+     * x0 y0 x1 y1：渐变的两个端点的位置
+     * color0 color1 是端点的颜色
+     * tile：端点范围之外的着色规则，类型是 TileMode。TileMode 一共有 3 个值可选：
+     * CLAMP, MIRROR 和  REPEAT。
+     * CLAMP （夹子模式？？？算了这个词我不会翻）会在端点之外延续端点处的颜色；
+     * MIRROR 是镜像模式；REPEAT 是重复模式。具体的看一下例子就明白。
+     */
+    Shader shader = new LinearGradient(
+            100f, 100f, 500f, 500f,
+            Color.parseColor("#E91E63"), Color.parseColor("#2196F3"),
+            Shader.TileMode.REPEAT
+    );
 
     public Practice01LinearGradientView(Context context) {
         super(context);
@@ -31,6 +48,9 @@ public class Practice01LinearGradientView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+
+        paint.setShader(shader);
 
         canvas.drawCircle(300, 300, 200, paint);
     }
