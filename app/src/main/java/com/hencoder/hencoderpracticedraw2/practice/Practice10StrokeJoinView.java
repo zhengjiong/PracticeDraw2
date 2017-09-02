3d4773c1ef2947e8163b8f7eb6d5ae84eb744ec9
zhengjiong <zhengjiong2006@gmail.com>
2017-09-02 16:57:24 +0800

    feat<Practice>finish 10

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice10StrokeJoinView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice10StrokeJoinView.java
@@ -38,18 +38,25 @@ public class Practice10StrokeJoinView extends View {
 
         canvas.save();
 
-        // 使用 Paint.setStrokeJoin() 来设置不同的拐角形状
+
+        /**
+         * 使用 Paint.setStrokeJoin() 来设置不同的拐角形状
+         * 设置拐角的形状。有三个值可以选择：MITER 尖角、 BEVEL 平角和 ROUND 圆角。默认为 MITER。
+         */
 
         canvas.translate(100, 100);
         // 第一种形状：MITER
+        paint.setStrokeJoin(Paint.Join.MITER);
         canvas.drawPath(path, paint);
 
         canvas.translate(300, 0);
         // 第二种形状：BEVEL
+        paint.setStrokeJoin(Paint.Join.BEVEL);
         canvas.drawPath(path, paint);
 
         canvas.translate(300, 0);
         // 第三种形状：ROUND
+        paint.setStrokeJoin(Paint.Join.ROUND);
         canvas.drawPath(path, paint);
 
         canvas.restore();
