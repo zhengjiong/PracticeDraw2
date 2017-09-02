e0ec505bab46211cc09be7f43d306da82b75e2bc
zhengjiong <zhengjiong2006@gmail.com>
2017-09-02 16:31:30 +0800

    feat<Practice>finish 07

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice07ColorMatrixColorFilterView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice07ColorMatrixColorFilterView.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -14,6 +17,7 @@ import com.hencoder.hencoderpracticedraw2.R;
 public class Practice07ColorMatrixColorFilterView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap;
+    ColorFilter colorFilter;
 
     public Practice07ColorMatrixColorFilterView(Context context) {
         super(context);
@@ -32,6 +36,17 @@ public class Practice07ColorMatrixColorFilterView extends View {
 
         // 使用 setColorFilter() 设置一个 ColorMatrixColorFilter
         // 用 ColorMatrixColorFilter.setSaturation() 把饱和度去掉
+
+        /**
+         * ColorMatrix 有一些自带的方法可以做简单的转换，例如可以使用 setSaturation(float sat) 来设置饱和度；
+         * 另外你也可以自己去设置它的每一个元素来对转换效果做精细调整。具体怎样设置会有怎样的效果，我就不讲了（其实是我也不太会😅）。
+         * 如果你有需求，可以试一下程大治同学做的这个库：StyleImageView,https://github.com/chengdazhi/StyleImageView
+         */
+
+        ColorMatrix matrix = new ColorMatrix();
+        matrix.setSaturation(0);//设置饱和度为0
+        colorFilter = new ColorMatrixColorFilter(matrix);
+        paint.setColorFilter(colorFilter);
     }
 
     @Override
