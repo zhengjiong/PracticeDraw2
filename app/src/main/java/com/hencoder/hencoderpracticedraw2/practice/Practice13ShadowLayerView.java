adbb5afafc3f1b09d40b88fb18da41c37743a34a
zhengjiong <zhengjiong2006@gmail.com>
2017-09-03 22:41:39 +0800

    feat<Practice>finish 14 setMaskFilter

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice13ShadowLayerView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice13ShadowLayerView.java
@@ -32,9 +32,7 @@ public class Practice13ShadowLayerView extends View {
          如果要清除阴影层，使用 clearShadowLayer() 。
 
          注意：
-
          在硬件加速开启的情况下， setShadowLayer() 只支持文字的绘制，文字之外的绘制必须关闭硬件加速才能正常绘制阴影。
-
          如果 shadowColor 是半透明的，阴影的透明度就使用 shadowColor 自己的透明度；而如果 shadowColor 是不透明的，阴影的透明度就使用 paint 的透明度。
          */
         paint.setShadowLayer(14, 9, 9, Color.GRAY);
--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw2.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
@@ -38,17 +39,35 @@ public class Practice14MaskFilterView extends View {
         super.onDraw(canvas);
 
         // 用 Paint.setMaskFilter 来设置不同的 BlurMaskFilter
+        /**
+         * 为之后的绘制设置 MaskFilter。上一个方法 setShadowLayer() 是设置的在绘制层下方的附加效果；
+         * 而这个  MaskFilter 和它相反，设置的是在绘制层上方的附加效果。
+
+         到现在已经有两个 setXxxFilter(filter) 了。前面有一个 setColorFilter(filter) ，
+         是对每个像素的颜色进行过滤；而这里的 setMaskFilter(filter) 则是基于整个画面来进行过滤。
+         MaskFilter 有两种： BlurMaskFilter 和 EmbossMaskFilter。
+
+         NORMAL: 内外都模糊绘制
+         SOLID: 内部正常绘制，外部模糊
+         INNER: 内部模糊，外部不绘制
+         OUTER: 内部不绘制，外部模糊（什么鬼？）
+         */
+
 
         // 第一个：NORMAL
+        paint.setMaskFilter(new BlurMaskFilter(60, BlurMaskFilter.Blur.NORMAL));
         canvas.drawBitmap(bitmap, 100, 50, paint);
 
         // 第二个：INNER
+        paint.setMaskFilter(new BlurMaskFilter(60, BlurMaskFilter.Blur.INNER));
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, 50, paint);
 
         // 第三个：OUTER
+        paint.setMaskFilter(new BlurMaskFilter(60, BlurMaskFilter.Blur.OUTER));
         canvas.drawBitmap(bitmap, 100, bitmap.getHeight() + 100, paint);
 
         // 第四个：SOLID
+        paint.setMaskFilter(new BlurMaskFilter(60, BlurMaskFilter.Blur.SOLID));
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, bitmap.getHeight() + 100, paint);
     }
 }
