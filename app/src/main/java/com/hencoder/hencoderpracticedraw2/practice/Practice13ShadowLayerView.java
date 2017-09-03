3b41e5e20670c9fcab7ddd13f9f9537d35e405ac
zhengjiong <zhengjiong2006@gmail.com>
2017-09-03 22:18:59 +0800

    feat<Practice>finish 13 shadowLayer

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice13ShadowLayerView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice13ShadowLayerView.java
@@ -2,6 +2,7 @@ package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -24,6 +25,20 @@ public class Practice13ShadowLayerView extends View {
 
     {
         // 使用 Paint.setShadowLayer() 设置阴影
+        /**
+         * 在之后的绘制内容下面加一层阴影。方法的参数里，
+         * radius 是阴影的模糊范围； dx dy 是阴影的偏移量； shadowColor 是阴影的颜色。
+
+         如果要清除阴影层，使用 clearShadowLayer() 。
+
+         注意：
+
+         在硬件加速开启的情况下， setShadowLayer() 只支持文字的绘制，文字之外的绘制必须关闭硬件加速才能正常绘制阴影。
+
+         如果 shadowColor 是半透明的，阴影的透明度就使用 shadowColor 自己的透明度；而如果 shadowColor 是不透明的，阴影的透明度就使用 paint 的透明度。
+         */
+        paint.setShadowLayer(14, 9, 9, Color.GRAY);
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
     }
 
     @Override
@@ -32,5 +47,9 @@ public class Practice13ShadowLayerView extends View {
 
         paint.setTextSize(120);
         canvas.drawText("Hello HenCoder", 50, 200, paint);
+
+        paint.setStyle(Paint.Style.FILL);
+        paint.setShadowLayer(20, 0, 20, Color.GRAY);
+        canvas.drawCircle(300, 500, 200, paint);
     }
 }
